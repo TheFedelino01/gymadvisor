d2130ef12ce71a76bd09cb320f474c6fb46285a8
TheFedelino01 <federico.saccani01@gmail.com>
2020-09-06 16:50:09 +0200

    Grafica corretta modal Login Bootstrap

--- index.php
+++ index.php
@@ -29,8 +29,10 @@
 
 .grande{
 	font-size:30px;
-	font-size: 1.9rem;	
-	
+	font-size: 1.9rem;		
+}
+.medio{
+	font-size: 20px;
 }
 
 .logo{
@@ -47,7 +49,7 @@
 	top:50%;
 }
 
-.btn{
+.btnMio{
 	display: inline-block;
     border: 1px solid #000;
     padding: 10px 15px;
@@ -98,99 +100,6 @@ body[modalita="smartphone"] #searchbar{
 
 
 
-<!-- Roba login -->
-body {font-family: Arial, Helvetica, sans-serif;}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-
-/* Set a style for all buttons */
-
-button:hover {
-  opacity: 0.8;
-}
-
-/* Extra styles for the cancel button */
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #f44336;
-}
-
-/* Center the image and position the close button */
-.imgcontainer {
-  text-align: center;
-  margin: 24px 0 12px 0;
-  position: relative;
-}
-
-img.avatar {
-  width: 40%;
-  border-radius: 50%;
-}
-
-.container {
-  padding: 16px;
-}
-
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
-
-
-
-/* The Close Button (x) */
-.close {
-  position: absolute;
-  right: 25px;
-  top: 0;
-  color: #000;
-  font-size: 35px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: red;
-  cursor: pointer;
-}
-
-/* Add Zoom Animation */
-.animate {
-  -webkit-animation: animatezoom 0.6s;
-  animation: animatezoom 0.6s
-}
-
-@-webkit-keyframes animatezoom {
-  from {-webkit-transform: scale(0)} 
-  to {-webkit-transform: scale(1)}
-}
-  
-@keyframes animatezoom {
-  from {transform: scale(0)} 
-  to {transform: scale(1)}
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
-}
-
-<!-- Roba login -->
 </style>
 
 <script>        
@@ -232,7 +141,7 @@ window.addEventListener("resize", displayWindowSize);
 			</div>
 			<div id="navRight" class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 allineaDx" style="padding-top:22px;">
 				
-				<button type="button" class="ml-auto mr-auto readmore btn" data-toggle="modal" data-target="#exampleModal">
+				<button type="button" class="ml-auto mr-auto readmore btnMio" data-toggle="modal" data-target="#exampleModal">
 				Login
 				</button>
 			</div>
@@ -268,42 +177,29 @@ window.addEventListener("resize", displayWindowSize);
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Login Account</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-          <form class="modal-content animate" action="/action_page.php" method="post">
-			<div class="imgcontainer">
-			  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-			  <img class="logo" src="./img/logo.png" style="vertical-align: bottom;">
-			</div>
-
-			<div class="container">
-			  <label for="uname"><b>Username</b></label>
-			  <input type="text" placeholder="Enter Username" name="uname" required>
-
-			  <label for="psw"><b>Password</b></label>
-			  <input type="password" placeholder="Enter Password" name="psw" required>
-				
-			  <button type="submit">Login</button>
-			  <label>
-				<input type="checkbox" checked="checked" name="remember"> Remember me
-			  </label>
-			</div>
-
-			<div class="container" style="background-color:#f1f1f1">
-			  <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-			  <span class="psw">Forgot <a href="#">password?</a></span>
-			</div>
-		  </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
+		<form>
+		  <div class="modal-body" >
+				  <div class="form-group">
+					<label for="exampleInputEmail1" class="medio">Email:</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+				  </div>
+				  <div class="form-group">
+					<label for="exampleInputPassword1" class="medio">Password</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				  </div>
+		  </div>
+		  <div class="modal-footer" style="padding-bottom: 0px;">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
+			<button type="submit" class="btn btn-primary">Entra</button>
+		  </div>
+		  
+		</form>
+	</div>
   </div>
 </div>
 
